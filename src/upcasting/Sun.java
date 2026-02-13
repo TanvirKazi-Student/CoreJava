ce2be50db39c9fa634943482011aa5b8032db0d2
Tanvir kazi <148249195+TanvirKazi-Student@users.noreply.github.com>
2026-02-13 19:22:01 +0530

    Rename method 'ested' to 'proparty' in Sun class
    Refactored method 'ested' to 'proparty' and added @override annotation.
    
    Signed-off-by: Tanvir kazi <148249195+TanvirKazi-Student@users.noreply.github.com>

--- src/upcasting/Sun.java
+++ src/upcasting/Sun.java
@@ -1,17 +1,18 @@
 package upcasting;
 
 public class Sun extends Father {
-	public void ested() {
+	@override
+	public void proparty() {
 		System.out.println("Suns ested");
 	}
 
 	public static void main(String[] args) {
 		Father f1 = new Sun();
 		f1.proparty();// this is the example of upcasting
-//		f1.ested();
+//		f1.proparty();
 
 		Sun s2 = (Sun) f1;//this is the example of downcasting
-        s2.ested();
+        s2.proparty();
         s2.proparty();
 	}
 
