0fd1480bba311e8b0a5b910a9daf3a5339eb252d
Tanvir kazi <148249195+TanvirKazi-Student@users.noreply.github.com>
2026-02-13 19:36:28 +0530

    Update Bank.java
    Signed-off-by: Tanvir kazi <148249195+TanvirKazi-Student@users.noreply.github.com>

--- src/trowsexception/Bank.java
+++ src/trowsexception/Bank.java
@@ -1,12 +1,12 @@
 package trowsexception;
 
-public class Bank implements Cloneable {
+public class Bank implements Cloneable { //implements Cloneable is not write here then throws CloneNotSupportedException  
 
 	public static void main(String[] args) throws CloneNotSupportedException  {
 		Bank b1 = new Bank();
 		Bank b2 =  (Bank) b1.clone();
 		
-		System.out.println(b1.equals(b2));
+		System.out.println(b1.equals(b2));//f
 	
 //		b1.getClass();
 //		b1.hashCode();
